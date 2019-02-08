96ae3456b519ba111f1b7ceea90e2c702d91e216
anakdesa088 <anakdesa088@gmail.com>
2019-02-08 20:18:36 +0700

    Fix menu

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -1,49 +1,55 @@
             <div class="main-container">
-                <div class="sidebar-main">
-                    <nav class="sidebar-nav">
-                        <ul class="metismenu" id="side-menu">
-                             <li class="active">
-                              <a href="javascript:void(0)"><i class="ti-dashboard"></i> <span class="menu-label">Dashboard</span></a>
-                                
-                            </li>
-                            
-                          <li>
-                         <a href="javascript:void(0)"><i class="fa fa-building"></i> <span class="menu-label">Akademik</span><span class="fa arrow"></span></a>
-                                <ul class="nav nav-sub collapse">
-                                    <li><a href="ui-buttons.html"> Sara</a></li>
-                                    <li ><a href="ui-panels.html"> Panels</a></li>
-                                    <li ><a href="ui-dragabble-panels.html">Draggable Panels</a></li>
-                                    <li ><a href="ui-tabs-accordion.html"> Tabs &amp; Accordion</a></li>
-                                    
-                                </ul>
-                            </li>
+              <div class="sidebar-main">
+                <nav class="sidebar-nav">
+                  <ul class="metismenu" id="side-menu">
+                   <li class="active">
+                    <a href="javascript:void(0)"><i class="ti-dashboard"></i> <span class="menu-label">Dashboard</span></a>
 
-                            <li>
-                                <a href="javascript:void(0)"><i class="fa fa-money"></i> <span class="menu-label">Keuangan</span><span class="fa arrow"></span></a>
-                                <ul class="nav nav-sub collapse">
-                                <li><a href="<?php echo site_url('backend/dashboard/keuangan'); ?>"> Data PMB</a></li>
-                                
-                                </ul>
-                            </li>
+                  </li>
+
+                  <li>
+                   <a href="javascript:void(0)"><i class="fa fa-building"></i> <span class="menu-label">AKADEMIK</span><span class="fa arrow"></span></a>
+                   <ul class="nav nav-sub collapse">
+                    <li><a href="ui-buttons.html"> PMB</a></li>
+                    <li ><a href="ui-panels.html"> DATA KELAS</a></li>
+                    <li ><a href="ui-panels.html"> DATA MAHASISWA</a></li>
+                    <li ><a href="ui-panels.html"> DATA DOSEN</a></li>
+                    <li ><a href="ui-panels.html"> DATA KELAS</a></li>
+                    <li ><a href="ui-panels.html"> DATA MATA KULIAH</a></li>
+                    <li ><a href="ui-panels.html"> CETAK KRS</a></li>
+                    <li ><a href="ui-panels.html"> CETAK KHS</a></li>
+                  </ul>
+                </li>
+
+                <li>
+                  <a href="javascript:void(0)"><i class="fa fa-money"></i> <span class="menu-label">KEUANGAN</span><span class="fa arrow"></span></a>
+                  <ul class="nav nav-sub collapse">
+                    <li><a href="<?php echo site_url('backend/dashboard/keuangan'); ?>"> PMB</a></li>
+                    <li><a href="<?php echo site_url('backend/dashboard/keuangan'); ?>"> JENIS BIAYA</a></li>
+                    <li><a href="<?php echo site_url('backend/dashboard/keuangan'); ?>"> PEMBAYARAN MAHASISWA</a></li>
+                    <li><a href="<?php echo site_url('backend/dashboard/keuangan'); ?>"> GAJI DOSEN</a></li>
+
+                  </ul>
+                </li>
 
 
 
-                            <li>
-                           <a href="javascript:void(0)"><i class="fa fa-users"></i> <span class="menu-label">Mahasiswa</span><span class="fa arrow"></span></a>
-                                <ul class="nav nav-sub collapse">
-                                <li><a href="google-map.html"> Google Map</a></li>
-                                <li><a href="vector-map.html"> Vector Map</a></li>
-                               </ul>
-                            </li>
+                <li>
+                 <a href="javascript:void(0)"><i class="fa fa-users"></i> <span class="menu-label">Mahasiswa</span><span class="fa arrow"></span></a>
+                 <ul class="nav nav-sub collapse">
+                  <li><a href="google-map.html"> Google Map</a></li>
+                  <li><a href="vector-map.html"> Vector Map</a></li>
+                </ul>
+              </li>
 
-							<li><a href="widget.html"><i class="fa fa-user-circle-o"></i>  <span class="menu-label">Dosen </span></a></li>
-                            <li><a href="calendar.html"><i class="fa fa-pencil-square-o"></i>  <span class="menu-label">Ujian </span></a></li>
-                            <li><a href="calendar.html"><i class="fa fa-envelope-open-o"></i>  <span class="menu-label">Pesan </span></a></li>
-                            <li><a href="calendar.html"><i class="fa fa-calendar"></i>  <span class="menu-label">Calendar </span></a></li>
-                            
-                            
-                        </ul>
-                    </nav>
-                </div>
-               <!-- End Sidebar Main-->
+              <li><a href="widget.html"><i class="fa fa-user-circle-o"></i>  <span class="menu-label">Dosen </span></a></li>
+              <li><a href="calendar.html"><i class="fa fa-pencil-square-o"></i>  <span class="menu-label">Ujian </span></a></li>
+              <li><a href="calendar.html"><i class="fa fa-envelope-open-o"></i>  <span class="menu-label">Pesan </span></a></li>
+              <li><a href="calendar.html"><i class="fa fa-calendar"></i>  <span class="menu-label">Calendar </span></a></li>
+
+
+            </ul>
+          </nav>
+        </div>
+        <!-- End Sidebar Main-->
 
