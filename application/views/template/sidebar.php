5755338c15ee29d418db4348c9a557fe147a75ad
anakdesa088 <anakdesa088@gmail.com>
2019-02-13 18:35:44 +0700

    merubah di assetss

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -1,19 +1,15 @@
-<aside class="left-sidebar">
-            <!-- Sidebar scroll-->
-            <div class="scroll-sidebar">
-                
-                
-                
-                <nav class="sidebar-nav">
-                    <ul id="sidebarnav">
-                        <li class="nav-devider"></li>
-                        <li class="nav-small-cap"></li>
-                        <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-gauge"></i>Dashboard</a>
+            <div class="main-container">
+                <div class="sidebar-main">
+                    <nav class="sidebar-nav">
+                        <ul class="metismenu" id="side-menu">
+                             <li class="active">
+                              <a href="javascript:void(0)"><i class="ti-dashboard"></i> <span class="menu-label">Dashboard</span></a>
+                                
+                            </li>
                             
-                        </li>
-                        
-                        <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="fa fa-building"></i><span class="hide-menu">AKADEMIK </span></a>
-                            <ul aria-expanded="false" class="collapse">
+                          <li>
+                         <a href="javascript:void(0)"><i class="ti-new-window"></i> <span class="menu-label">Akademik</span><span class="fa arrow"></span></a>
+                                <ul class="nav nav-sub collapse">
                                 <li><a href="<?php echo site_url('backend/akademik/index'); ?>">PMB</a></li>
                                 <li><a href="apa.html">DATA KELAS</a></li>
                                 <li><a href="apa.html">DATA MAHASISWA</a></li>
@@ -21,68 +17,38 @@
                                 <li><a href="apa.html">DATA MATA KULIAH</a></li>
                                 <li><a href="apa.html">CETAK KRS</a></li>
                                 <li><a href="apa.html">CETAK KHS</a></li>
-                            </ul>
-                        </li>
-                        <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="fa fa-money"></i><span class="hide-menu">KEUANGAN </span></a>
-                            <ul aria-expanded="false" class="collapse">
+                                    
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="javascript:void(0)"><i class="ti-email"></i> <span class="menu-label">Keuangan</span><span class="fa arrow"></span></a>
+                                <ul class="nav nav-sub collapse">
                                 <li><a href="<?php echo site_url('backend/keuangan/index'); ?>">PMB</a></li>
                                 <li><a href="APA.HTML">JENIS BIAYA</a></li>
                                 <li><a href="APA.HTML">PEMBAYARAN MAHASISWA</a></li>
                                 <li><a href="APA.HTML">GAJI DOSEN</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-small-cap">FORMS, TABLE &amp; WIDGETS</li>
-                        
-                        
-                        
-                        <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-file-chart"></i><span class="hide-menu">Charts</span></a>
-                            <ul aria-expanded="false" class="collapse">
-                                <li><a href="chart-morris.html">Morris Chart</a></li>
-                                <li><a href="chart-chartist.html">Chartis Chart</a></li>
-                                <li><a href="chart-echart.html">Echarts</a></li>
-                                <li><a href="chart-flot.html">Flot Chart</a></li>
-                                <li><a href="chart-knob.html">Knob Chart</a></li>
-                                <li><a href="chart-chart-js.html">Chartjs</a></li>
-                                <li><a href="chart-sparkline.html">Sparkline Chart</a></li>
-                                <li><a href="chart-extra-chart.html">Extra chart</a></li>
-                                <li><a href="chart-peity.html">Peity Charts</a></li>
-                            </ul>
-                        </li>
-                        <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-brush"></i><span class="hide-menu">Icons</span></a>
-                            <ul aria-expanded="false" class="collapse">
-                                <li><a href="icon-material.html">Material Icons</a></li>
-                                <li><a href="icon-fontawesome.html">Fontawesome Icons</a></li>
-                                <li><a href="icon-themify.html">Themify Icons</a></li>
-                                <li><a href="icon-linea.html">Linea Icons</a></li>
-                                <li><a href="icon-weather.html">Weather Icons</a></li>
-                                <li><a href="icon-simple-lineicon.html">Simple Lineicons</a></li>
-                                <li><a href="icon-flag.html">Flag Icons</a></li>
-                            </ul>
-                        </li>
-                        <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-map-marker"></i><span class="hide-menu">Maps</span></a>
-                            <ul aria-expanded="false" class="collapse">
-                                <li><a href="map-google.html">Google Maps</a></li>
-                                <li><a href="map-vector.html">Vector Maps</a></li>
-                            </ul>
-                        </li>
-                        <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-arrange-send-backward"></i><span class="hide-menu">Multi level dd</span></a>
-                            <ul aria-expanded="false" class="collapse">
-                                <li><a href="#">item 1.1</a></li>
-                                <li><a href="#">item 1.2</a></li>
-                                <li> <a class="has-arrow" href="#" aria-expanded="false">Menu 1.3</a>
-                                    <ul aria-expanded="false" class="collapse">
-                                        <li><a href="#">item 1.3.1</a></li>
-                                        <li><a href="#">item 1.3.2</a></li>
-                                        <li><a href="#">item 1.3.3</a></li>
-                                        <li><a href="#">item 1.3.4</a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="#">item 1.4</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </nav>
-                <!-- End Sidebar navigation -->
-            </div>
-            <!-- End Sidebar scroll-->
-        </aside>
+                                
+                                </ul>
+                            </li>
+
+
+
+                            <li>
+                           <a href="javascript:void(0)"><i class="icon-location-pin"></i> <span class="menu-label">Maps</span><span class="fa arrow"></span></a>
+                                <ul class="nav nav-sub collapse">
+                                <li><a href="google-map.html"> Google Map</a></li>
+                                <li><a href="vector-map.html"> Vector Map</a></li>
+                               </ul>
+                            </li>
+
+                            <li><a href="widget.html"><i class="icon-docs"></i>  <span class="menu-label">Widget </span></a></li>
+                            <li><a href="calendar.html"><i class="fa fa-calendar"></i>  <span class="menu-label">Calendar </span></a></li>
+                            
+                            
+                        </ul>
+                    </nav>
+                </div>
+               <!-- End Sidebar Main-->
+
+                <!--Start wrapperr-->
