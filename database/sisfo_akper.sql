2f90912b79ac1635f660a28574254a6058f4eae3
anakdesa088 <anakdesa088@gmail.com>
2019-02-14 09:32:22 +0700

    database baru di dir /database/sisfo_akper.sql

--- database/sisfo_akper.sql
+++ database/sisfo_akper.sql
@@ -3,8 +3,8 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 08 Feb 2019 pada 13.37
--- Versi Server: 5.6.16
+-- Generation Time: Feb 14, 2019 at 03:30 AM
+-- Server version: 5.6.16
 -- PHP Version: 5.5.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -23,47 +23,102 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pendaftaran`
+-- Table structure for table `mhs`
 --
 
-CREATE TABLE IF NOT EXISTS `pendaftaran` (
-  `id_pendaftar` int(11) NOT NULL AUTO_INCREMENT,
-  `nama_lengkap` varchar(80) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `kewarganegaraan` varchar(20) NOT NULL,
-  `jenis_kelamin` varchar(10) NOT NULL,
+CREATE TABLE IF NOT EXISTS `mhs` (
+  `id_pmb` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_lengkap` varchar(100) NOT NULL,
+  `email` varchar(70) NOT NULL,
+  `password` varchar(70) NOT NULL,
+  `kewarganegaraan` varchar(40) NOT NULL,
+  `jk` varchar(20) NOT NULL,
   `tinggi_badan` int(11) NOT NULL,
   `berat_badan` int(11) NOT NULL,
-  `tempat_lhr` varchar(40) NOT NULL,
-  `tanggal_lhr` date NOT NULL,
-  `almt_lengkap` text NOT NULL,
+  `tmpt_lahir` varchar(40) NOT NULL,
+  `tgl_lahir` date NOT NULL,
+  `alamat` text NOT NULL,
   `kode_pos` int(11) NOT NULL,
-  `no_hp` int(11) NOT NULL,
+  `nama_ayah` varchar(40) NOT NULL,
+  `nama_ibu` varchar(40) NOT NULL,
+  `no_hp1` int(11) NOT NULL,
+  `no_hp2` int(11) NOT NULL,
   `info_dari` varchar(30) NOT NULL,
-  `nama_asal_sklh` varchar(60) NOT NULL,
-  `almt_asal_sklh` text NOT NULL,
-  `kab_asal_sklh` int(90) NOT NULL,
-  `foto_ijaza` int(100) NOT NULL,
-  `surat_tidak_buta_warna` varchar(100) NOT NULL,
-  `foto_pendaftar` varchar(100) NOT NULL,
-  `ayah` varchar(70) NOT NULL,
-  `ibu` varchar(70) NOT NULL,
-  `level` enum('mahasiswa','mahasiswa_baru') NOT NULL,
-  PRIMARY KEY (`id_pendaftar`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=46 ;
+  `nama_asal_sekolah` varchar(70) NOT NULL,
+  `alamat_asal_sekolah` text NOT NULL,
+  `foto` varchar(100) NOT NULL,
+  `foto_ijaza` varchar(100) NOT NULL,
+  `foto_kesehatan` varchar(100) NOT NULL,
+  `level` enum('pmb_baru','pmb_lamah') NOT NULL,
+  PRIMARY KEY (`id_pmb`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data untuk tabel `pendaftaran`
+-- Table structure for table `pmb`
 --
 
-INSERT INTO `pendaftaran` (`id_pendaftar`, `nama_lengkap`, `email`, `password`, `kewarganegaraan`, `jenis_kelamin`, `tinggi_badan`, `berat_badan`, `tempat_lhr`, `tanggal_lhr`, `almt_lengkap`, `kode_pos`, `no_hp`, `info_dari`, `nama_asal_sklh`, `almt_asal_sklh`, `kab_asal_sklh`, `foto_ijaza`, `surat_tidak_buta_warna`, `foto_pendaftar`, `ayah`, `ibu`, `level`) VALUES
-(40, 'Saefudin Hardi', '', '', 'WNI', 'Perempuan', 928, 928, 'Kab. Cirebon', '2018-11-12', 'Dusun 05 RT/RW : 017/005 Bayalangu Lor, Gegesik', 45164, 2147483647, 'Temen', 'Smk Mekanika buntet', 'Bayalangu', 0, 0, '', '', 'Nasoni', 'Rasi', 'mahasiswa'),
-(41, '', 'casgoniedan@yahoo.com', '236874507', 'WNA', 'Laki-Laki', 0, 0, '', '2019-02-07', '', 0, 0, '', '', '', 0, 0, '', '', '', '', 'mahasiswa'),
-(42, 'anam', 'anam@gmail.com', '2124842993', 'WNI', 'Perempuan', 234, 234, 'Jakarta', '2018-12-06', 'saasasa', 11320, 2147483647, 'Temen', 'Smk Mekanika buntet', 'das', 0, 0, '', '', 'Nasoni', 'Rasi', 'mahasiswa'),
-(43, 'Saefudin Hardi', 'casgoniedan@yahoo.com', '1187887686', 'on', 'Laki-Laki', 12, 12, 'Cirebon', '2019-08-02', 'Dusun 05 RT/RW : 017/005 Bayalangu Lor, Gegesik', 45164, 2147483647, 'Saudara', 'Smk Mekanika buntet', 'Asepp', 0, 0, '', '', 'Nasoni', 'Rasi', 'mahasiswa'),
-(44, 'Saefudin Hardi', 'mikarta@yahoo.com', '1454329302', 'on', 'Laki-Laki', 23, 23, 'Kab. Cirebon', '2019-08-02', 'Dusun 05 RT/RW : 017/005 Bayalangu Lor, Gegesik', 45164, 2147483647, 'Temen', 'Smk Mekanika buntet', 'hjhj', 0, 0, '', '', 'Nasoni', 'Rasi', 'mahasiswa'),
-(45, '', 'mikarta@yahoo.com', '726373667', '', '', 0, 0, '', '0000-00-00', '', 0, 0, '', '', '', 0, 0, '', '', '', '', 'mahasiswa');
+CREATE TABLE IF NOT EXISTS `pmb` (
+  `id_pmb` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_lengkap` varchar(100) NOT NULL,
+  `email` varchar(70) NOT NULL,
+  `password` varchar(70) NOT NULL,
+  `kewarganegaraan` varchar(40) NOT NULL,
+  `jk` varchar(20) NOT NULL,
+  `tinggi_badan` int(11) NOT NULL,
+  `berat_badan` int(11) NOT NULL,
+  `tmpt_lahir` varchar(40) NOT NULL,
+  `tgl_lahir` date NOT NULL,
+  `alamat` text NOT NULL,
+  `kode_pos` int(11) NOT NULL,
+  `nama_ayah` varchar(40) NOT NULL,
+  `nama_ibu` varchar(40) NOT NULL,
+  `no_hp1` int(11) NOT NULL,
+  `no_hp2` int(11) NOT NULL,
+  `info_dari` varchar(30) NOT NULL,
+  `nama_asal_sekolah` varchar(70) NOT NULL,
+  `alamat_asal_sekolah` text NOT NULL,
+  `foto` varchar(100) NOT NULL,
+  `foto_ijaza` varchar(100) NOT NULL,
+  `foto_kesehatan` varchar(100) NOT NULL,
+  `status` enum('invalid','valid') NOT NULL,
+  `level` enum('pmb_baru','pmb_lamah') NOT NULL,
+  PRIMARY KEY (`id_pmb`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
+
+--
+-- Dumping data for table `pmb`
+--
+
+INSERT INTO `pmb` (`id_pmb`, `nama_lengkap`, `email`, `password`, `kewarganegaraan`, `jk`, `tinggi_badan`, `berat_badan`, `tmpt_lahir`, `tgl_lahir`, `alamat`, `kode_pos`, `nama_ayah`, `nama_ibu`, `no_hp1`, `no_hp2`, `info_dari`, `nama_asal_sekolah`, `alamat_asal_sekolah`, `foto`, `foto_ijaza`, `foto_kesehatan`, `status`, `level`) VALUES
+(3, 'Saefudin Hardi', 'mikarta@yahoo.com', '72105179', 'Wni', 'Laki-Laki', 23, 399, 'cirebon', '2019-12-31', 'Dusun 05 RT/RW : 017/005 Bayalangu Lor, Gegesik', 45164, 'Nasoni', 'rasi', 2147483647, 87384728, 'Teman', 'Smk NU mekanika', 'test\r\n', '', 'amal9.jpg', '', 'valid', 'pmb_lamah'),
+(12, 'Sefudin Hardi', 'casgoni@gmail.com', '1886638202', 'Wni', 'Laki-Laki', 12, 12, 'Cirebon', '1992-12-12', 'Desa Bayalangu Lor', 84328748, 'Nasoni', 'Rasih', 92384, 93829489, 'Teman', 'Smk nu Mekanika', 'Saaksla', 'amal3.jpg', 'amal.jpg', 'amal2.jpg', 'valid', 'pmb_lamah');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_lengkap` varchar(100) NOT NULL,
+  `email` varchar(90) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `jenis_kelamin` enum('Laki-laki','Perempuan') NOT NULL,
+  `level` enum('superadmin','akademik','keuangan') NOT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `nama_lengkap`, `email`, `password`, `jenis_kelamin`, `level`) VALUES
+(1, 'Mubarok Hasanuddin', 'mubarok@gmail.com', 'indonesia', 'Laki-laki', 'superadmin'),
+(2, 'Asep', 'asep@gmail.com', 'indonesia', 'Laki-laki', 'akademik'),
+(3, 'Bang Umar', 'umar@gmail.com', 'indonesia', 'Laki-laki', 'keuangan');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
